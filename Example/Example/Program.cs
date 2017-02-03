869b89786c12a892b6ac5cc10ddf7a3c845ef831
nalbandyan.1991 <nalbandyan.1991@bk.ru>
2017-02-03 23:42:30 +0400

    Middle version

---
+++ Example/Example/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Ano(7, 8);
+            Console.ReadLine();
+
+
+
+
+        }
+        public static void  Ano(int a,int b)
+        {
+            if (a > b)
+                Console.WriteLine(a); 
+            else
+                Console.WriteLine(b); ;
+             
+
+        }
+    }
+}
--- GameTicTac/GameTicTac/Form1.cs
+++ GameTicTac/GameTicTac/Form1.cs
@@ -12,8 +12,6 @@ namespace GameTicTac
 {
     public partial class Form1 : Form
     {
-
-
         bool turn = true;
         int turn_count = 0;
         public Form1()
@@ -42,28 +40,26 @@ namespace GameTicTac
         }
         private void Chekwinner()
         {
-        
-        bool there_is_awinner= false;
+            bool there_is_awinner = false;
             //Chek horizontal winner
-            if ((A1.Text==A2.Text)&&(A2.Text==A3.Text)&&(!A1.Enabled))
-                there_is_awinner=true;
-            else if ((A4.Text==A5.Text)&&(A5.Text==A6.Text)&&(!A4.Enabled))
-                 there_is_awinner=true;
-            else if ((A7.Text==A8.Text)&&(A8.Text==A9.Text)&&(!A7.Enabled))
-                 there_is_awinner=true;
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+                there_is_awinner = true;
+            else if ((A4.Text == A5.Text) && (A5.Text == A6.Text) && (!A4.Enabled))
+                there_is_awinner = true;
+            else if ((A7.Text == A8.Text) && (A8.Text == A9.Text) && (!A7.Enabled))
+                there_is_awinner = true;
             //Chek vertical winner
-            if ((A1.Text == A4.Text) && (A4.Text == A7.Text)&&(!A1.Enabled))
-                 there_is_awinner = true;
-            else if ((A2.Text == A5.Text) && (A5.Text == A8.Text)&&(!A2.Enabled))
-                 there_is_awinner = true;
-            else if ((A3.Text == A6.Text) && (A6.Text == A9.Text)&&(!A3.Enabled))
-                 there_is_awinner = true;
+            if ((A1.Text == A4.Text) && (A4.Text == A7.Text) && (!A1.Enabled))
+                there_is_awinner = true;
+            else if ((A2.Text == A5.Text) && (A5.Text == A8.Text) && (!A2.Enabled))
+                there_is_awinner = true;
+            else if ((A3.Text == A6.Text) && (A6.Text == A9.Text) && (!A3.Enabled))
+                there_is_awinner = true;
             //Chek diaganal winner
             if ((A1.Text == A5.Text) && (A5.Text == A9.Text) && (!A1.Enabled))
                 there_is_awinner = true;
             else if ((A3.Text == A5.Text) && (A5.Text == A7.Text) && (!A7.Enabled))
                 there_is_awinner = true;
-
             if (there_is_awinner)
             {
                 DisabeleButton();
@@ -72,17 +68,14 @@ namespace GameTicTac
                     winner = "O";
                 else
                     winner = "X";
-                MessageBox.Show(winner+" "+"Wins");//Show winner name 
-             }
+                MessageBox.Show(winner + " " + "Wins");//Show winner name 
+            }
             else
             {
-                if (turn_count==9)
+                if (turn_count == 9)
                     MessageBox.Show("Draw" + " " + "wwwwww");
-
             }
-
-        }
-        
+        }               
         private void DisabeleButton()
         {
             try
@@ -90,12 +83,11 @@ namespace GameTicTac
                 foreach (Component c in Controls)
                 {
                     Button b = (Button)c;
-                    b.Enabled = false;
-                    
+                    b.Enabled = false;                    
                 }
             }
-            catch { }
-
+            catch {
+            }
         }
 
         private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
