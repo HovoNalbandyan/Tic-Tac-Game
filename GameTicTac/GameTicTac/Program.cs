3320bab555a41bd5a12e39397dd2c4d8761cb193
nalbandyan.1991 <nalbandyan.1991@bk.ru>
2017-02-04 23:40:16 +0400

    End

--- GameTicTac/GameTicTac/Program.cs
+++ GameTicTac/GameTicTac/Program.cs
@@ -8,9 +8,7 @@ namespace GameTicTac
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
